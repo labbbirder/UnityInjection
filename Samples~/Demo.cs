ea5cc7ef95fa7343ab7fe10881efc33ea1d3c4ad
bbbirder <502100554@qq.com>
2023-06-12 18:06:13 +0800

    chore: add sample

---
+++ Samples~/Demo.cs
@@ -0,0 +1,36 @@
+using com.bbbirder.unity;
+using UnityEngine;
+using System;
+using UnityEngine.Assertions;
+
+public class Demo:MonoBehaviour{
+    void Start(){
+        FixHelper.Install();
+    }
+    void Update(){
+        Debug.Log(DemoModal.Salute());
+        Debug.Log(new DemoModal().ThisSalute());
+        Debug.Log(new DemoModal().Add(4,1.2f));
+    }
+
+    #region Fix
+    static Func<int> RawSalute;
+    [Injection(typeof(DemoModal),nameof(DemoModal.Salute),nameof(RawSalute))]
+    static int Salute(){
+        return 2;
+    }
+
+    static Func<DemoModal,int> RawThisSalute;
+    [Injection(typeof(DemoModal),nameof(DemoModal.ThisSalute),nameof(RawThisSalute))]
+    static int ThisSalute(DemoModal demo){
+        return 2;
+    }
+
+    static Func<DemoModal,int,float,float> RawAdd;
+    [Injection(typeof(DemoModal),nameof(DemoModal.Add),nameof(RawAdd))]
+    static float Add(DemoModal demo,int i,float f){ 
+        Debug.Log($"Add {i} + {f} is {i+f}"); 
+        return i+f;
+    }
+    #endregion
+}
---
+++ Samples~/DemoModal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using com.bbbirder.unity;
+public class DemoModal{
+    public static int Salute(){
+        return 1;
+    }
+    public int ThisSalute(){
+        return 1;
+    }
+    public float Add(int i, float f){
+        return i*f;
+    }
+}
